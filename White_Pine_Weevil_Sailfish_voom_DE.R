658cdecfa8427c9e1cab9a33829f1916e8274188
jennybc <jenny@stat.ubc.ca>
2014-05-29 11:48:28 -0700

    Wimpy safety commit

--- White_Pine_Weevil_Sailfish_voom_DE.R
+++ White_Pine_Weevil_Sailfish_voom_DE.R
@@ -8,74 +8,83 @@ library(testthat) # facilitate tests that will catch changes on re-analysis
 ### Experiment with limma + voom
 
 # Load counts from Sailfish
-filteredSailfishCounts <- read.delim("consolidated-filtered-Sailfish-results.txt")
+filteredSailfishCounts <- # take a few moments
+  read.delim("consolidated-filtered-Sailfish-results.txt")
 str(filteredSailfishCounts) # 'data.frame':  65609 obs. of  24 variables:
 test_that("filtered Sailfish data has 65609 rows upon import",
           expect_equal(65609, nrow(filteredSailfishCounts)))
 test_that("Sailfish data has data for exactly 24 samples",
           expect_equal(24, ncol(filteredSailfishCounts)))
 
-# Load design matrix
-desMat <- read.delim("White_Pine_Weevil_design_matrix.tsv",
+# Load experimental design
+expDes <- read.delim("White_Pine_Weevil_exp_design.tsv",
                      stringsAsFactors = FALSE)
-desMat <-
-  mutate(desMat,
-         gTypeCode = factor(gTypeCode, levels = c("Q903", "H898")),
-         gType = factor(gType, levels = c("Susc", "Res")),
+expDes <-
+  mutate(expDes,
+         gType = factor(gType, levels = c("Q903susc", "H898res")),
          txCode = factor(txCode, levels = c('C', 'W', 'G')),
          tx = factor(tx, levels = c("Control", "Wound", "Gallery")))
-desMat$grp <-
-  with(desMat, factor(grp,
-                      levels = paste(levels(gTypeCode),
+expDes$grp <-
+  with(expDes, factor(grp,
+                      levels = paste(levels(gType),
                                      rep(levels(tx), each = 2), sep = ".")))
-str(desMat) # 'data.frame':  24 obs. of  7 variables:
-test_that("design matrix has 24 rows upon import", expect_equal(24, nrow(desMat)))
+str(expDes) # 'data.frame':  24 obs. of  6 variables:
+test_that("design matrix has 24 rows upon import",
+          expect_equal(24, nrow(expDes)))
 
 # Load counts into DGEList object from edgeR package.
-y <- DGEList(counts = filteredSailfishCounts, group = desMat$grp)
+y <- DGEList(counts = filteredSailfishCounts, group = expDes$grp)
 
 # TMM Normalization by Depth
 y <- calcNormFactors(y)
 
 # make model matrix
-#modMat <- model.matrix(~ gType/tx - 1, desMat)
-modMat <- model.matrix(~ tx/gType - 1, desMat)
-modMat <- model.matrix(~ gType * tx, desMat)
+#modMat <- model.matrix(~ tx/gType - 1, expDes)
+modMat <- model.matrix(~ gType * tx, expDes)
+
+# not sure why this is necessary but I do it to please makeContrasts
+colnames(modMat) <- gsub(":", ".", colnames(modMat))
 
 # voom transformation
-v <- voom(y, modMat, plot = TRUE)
+v <- voom(y, modMat, plot = TRUE) # take a couple moments
 
 # Linear modelling
 fit <- lmFit(v, modMat)
-fit <- eBayes(fit)
+fit2 <- eBayes(fit)
 
-tt <- topTable(fit, coef = grep(":", colnames(modMat)))
+# get inferential info on each interaction effect on its own
+tt_gTypeH898res.txWound <-
+  topTable(fit2, coef = grep("gTypeH898res.txWound", colnames(modMat)))
+head(tt_gTypeH898res.txWound)
 
-data.wide <- cbind(desMat, t(y$counts[rownames(tt)[1:4], ]))
+tt_gTypeH898res.txGallery <-
+  topTable(fit2, coef = grep("gTypeH898res.txGallery", colnames(modMat)))
+head(tt_gTypeH898res.txGallery)
+
+# create contrast matrix to take difference of interaction terms
+cont_matrix <-
+  makeContrasts(gTypeH898res.txGallery - gTypeH898res.txWound,
+                levels = modMat)
+fit3 <- contrasts.fit(fit, cont_matrix)
+fit4 <- eBayes(fit3)
+tt_diff_interactions <- topTable(fit4, coef = 1)
+head(tt)
+
+## I AM JUST ABOVE HERE
+## STILL NOT SATISFIED WITH EXTRACTION OF INFERENCE RE: DIFFERENCE OF THE INTERACTION TERMS
+
+## CODE BELOW HERE NOT FRESHED RECENTLY
+## SOME WRITTEN BY ME, SOME FRAGMENTS LEFT FROM MACK THAT I MIGHT WANT TO REVIVE
+
+data.wide <- cbind(expDes, t(y$counts[rownames(tt)[1:4], ]))
 data.tall <- melt(data.wide,
-                  id.vars = names(desMat),
+                  id.vars = names(expDes),
                   variable.name = 'contig', value.name = 'Expression')
 str(data.tall)
 
-#x <- data.frame(desMat, gExp = y$counts[rownames(tt)[1], ])
+#x <- data.frame(expDes, gExp = y$counts[rownames(tt)[1], ])
 p <- ggplot(data.tall, aes(x = gType, y = Expression))
 p + geom_point() + facet_grid(tx ~ contig)
-
-# Linear design matrix, simplest approach
-design <- model.matrix(~ 0 + grp, desMat)
-colnames(design) <- levels(desMat$grp)
-
-# Create contrast matrix
-cont.matrix <- makeContrasts(
-  Control.H898_vs_Q903 = H898.Control - Q903.Control,
-  Induced.H898_vs_Q903 = 
-    (H898.Gallery - (H898.Wound - H898.Control)) - 
-    (Q903.Gallery - (Q903.Wound - Q903.Control)),
-  levels = design)
-
-# Voom transformation
-v <- voom(y, modMat, plot = TRUE)
-
 
 
 # MDS analysis
@@ -93,12 +102,6 @@ ggplot(pca, aes(PC1, PC2, color = Group)) + geom_point(size=3) +
                               "Q903.Wound" = "#fc8d59")) + 
   labs(title="Voom + Limma Principal Component Analysis") + theme_bw()
 
-# Linear modelling
-fit <- lmFit(v, design)
-
-fit2 <- contrasts.fit(fit, cont.matrix)
-
-fit2 <- eBayes(fit2)
 
 linear.results <- decideTests(fit2, method="global", 
                               adjust.method="fdr", p.value=0.01)
