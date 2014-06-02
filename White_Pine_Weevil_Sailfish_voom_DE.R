fe5eb5198a16e72842aebb18b4456690323128b3
jennybc <jenny@stat.ubc.ca>
2014-06-02 13:24:01 -0700

    Add a term to the statistical inference results left behind
    Test if all terms involving genotype are equal to zero, simultaneously

--- White_Pine_Weevil_Sailfish_voom_DE.R
+++ White_Pine_Weevil_Sailfish_voom_DE.R
@@ -99,29 +99,50 @@ write.table(statInf_model_terms,
             quote = FALSE, sep = "\t", row.names = FALSE)
 
 # get inferential summary for the effects we are most interested in
-
-# below, we will rename gTypeH898res to control, to be parallel with would and
-# gallery
-focus_terms <- c("weevil", "gTypeH898res", "wound", "gallery")
-names(focus_terms) <- focus_terms
+focus_patterns <-
+  c("weevil", "gTypeH898res", "^gTypeH898res$", "wound", "gallery")
+focus_terms <-
+  c("weevil", "gTypeH898res_all", "gTypeH898res_in_control",
+    "gTypeH898res_in_wound", "gTypeH898res_in_gallery")
+names(focus_patterns) <- focus_terms
 statInf_focus_terms <-
-  adply(focus_terms, 1,
-        function(x) name_rows(topTable(fit3, coef = x,
+  alply(focus_patterns, 1,
+        function(x) name_rows(topTable(fit3,
+                                       coef = grep(x, colnames(coef(fit3))),
                                        number = Inf, sort.by = "none")))
+names(statInf_focus_terms) <- focus_terms
+
+#' I must massage these results before I can rbind them
+#' Must address fact that they don't have exactly the same variables
+var_names <- unique(unlist(llply(statInf_focus_terms, names)))
+ldply(statInf_focus_terms, function(x) {
+  y <- var_names %in% names(x)
+  names(y) <- var_names
+  y
+})
+
+#' get rid of the three separate estimates in the case where we are testing for
+#' equality with zero for three terms at once
+statInf_focus_terms$gTypeH898res_all <-
+  subset(statInf_focus_terms$gTypeH898res_all,
+         select = -c(gTypeH898res, gTypeH898res_txWound, gTypeH898res_txGallery))
+
+#' rbind them
+statInf_focus_terms <- ldply(statInf_focus_terms, function(x) x)
 statInf_focus_terms <-
   rename(statInf_focus_terms, c(X1 = "focus_term", .rownames = "contig"))
-statInf_focus_terms$focus_term <-
-  revalue(statInf_focus_terms$focus_term, c(gTypeH898res = "control"))
+summary(statInf_focus_terms)
 
-statInf_focus_terms <-
-  statInf_focus_terms[ , c("contig",
-                           setdiff(names(statInf_focus_terms),"contig"))]
+#' rearrange the variables
+vars_in_order <- c("contig", "focus_term", "logFC", "AveExpr",
+                   "t", "F", "P.Value", "adj.P.Val", "B")
+statInf_focus_terms <- statInf_focus_terms[vars_in_order]
 head(statInf_focus_terms)
 str(statInf_focus_terms)
 
 ## informal tests so we know if things change, differ for Jenny vs Mack, etc.
-test_that("stat inf on the focus terms has 262436 rows",
-          expect_equal(262436, nrow(statInf_focus_terms)))
+test_that("stat inf on the focus terms has 328045 rows",
+          expect_equal(328045, nrow(statInf_focus_terms)))
 (t_medians <- aggregate(t ~ focus_term, statInf_focus_terms, median))
 all.equal(t_medians$t, c(0.04817940, 0.157361758, 0.01198738, 0.10723831))
 # "Mean relative difference: 2.508257e-08" <-- that's OK!
