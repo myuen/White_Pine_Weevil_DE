93b74a708722353999a986717bbbd65f93acc95f
Mack Yuen <mack@glauca.msl.ubc.ca>
2014-06-24 11:33:11 -0700

    Update assertions to reflect the changes in the analysis.

--- analysis/helper03_load-focus-statinf.r
+++ analysis/helper03_load-focus-statinf.r
@@ -4,16 +4,21 @@
 load_focus_statInf <- function() {
   require(testthat)
   sidf <- read.delim("../results/limma-results-focus-terms.tsv")
-  test_that("inference results for our focus terms (still) have 328045 rows",
-            expect_equal(328045, nrow(sidf)))
-  sidf$focus_term <-
-    factor(sidf$focus_term,
-           levels = c("weevil", "gTypeH898res_all", "gTypeH898res_in_control",
-                      "gTypeH898res_in_wound", "gTypeH898res_in_gallery"))
+  test_that("inference results for our focus terms (still) have 459,263 rows",
+            expect_equal(459263, nrow(sidf)))
+  
+  sidf$focus_term <- 
+    factor(sidf$focus_term, levels = 
+             c("gTypeH898res_in_control", "gTypeH898res_in_wound",
+             "gTypeH898res_in_gallery", "gTypeH898res_all",
+             "weevil_in_gTypeQ903susc", "weevil_in_gTypeH898res", "weevil_diff"))
+
   (t_medians <- aggregate(t ~ focus_term, sidf, median))
   test_that("medians of the t statistics are what we expect",
             expect_equal(t_medians$t,
-                         c(0.04817940, 0.157361758, 0.01198738, 0.10723831),
+                         c(0.157361758, 0.01198738, 0.10723831,
+                           -0.15538291, -0.09072355, 0.04817940),
                          tolerance = 2 * .Machine$double.eps ^ 0.5))
+
   return(sidf)
 }
