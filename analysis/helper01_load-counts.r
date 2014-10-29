84262832cee54abda8701b0347abfde14400bfd9
Mack Yuen <mack@glauca.msl.ubc.ca>
2014-10-29 13:40:19 -0700

    Update assertions

--- analysis/helper01_load-counts.r
+++ analysis/helper01_load-counts.r
@@ -4,8 +4,8 @@ load_counts <- function() {
   require(testthat)  
   filteredSailfishCounts <- # take a few moments
     read.delim("../data/consolidated-filtered-Sailfish-results.txt")
-  test_that("filtered Sailfish data has 65609 rows upon import",
-            expect_equal(65609, nrow(filteredSailfishCounts)))
+  test_that("filtered Sailfish data has 65600 rows upon import",
+            expect_equal(65600, nrow(filteredSailfishCounts)))
   test_that("Sailfish data has data for exactly 24 samples",
             expect_equal(24, ncol(filteredSailfishCounts)))
   return(filteredSailfishCounts)
--- analysis/helper03_load-focus-statinf.r
+++ analysis/helper03_load-focus-statinf.r
@@ -3,22 +3,29 @@
 
 load_focus_statInf <- function() {
   require(testthat)
+  
   sidf <- read.delim("../results/limma-results-focus-terms.tsv")
-  test_that("inference results for our focus terms (still) have 459,263 rows",
-            expect_equal(459263, nrow(sidf)))
+  
+  test_that("inference results for our focus terms (still) have 825,917 rows",
+            expect_equal(65609 * 13, nrow(sidf)))
   
   sidf$focus_term <- 
     factor(sidf$focus_term, levels = 
-             c("gTypeH898res_in_control", "gTypeH898res_in_wound",
-             "gTypeH898res_in_gallery", "gTypeH898res_all",
-             "weevil_in_gTypeQ903susc", "weevil_in_gTypeH898res", "weevil_diff"))
+             c("wounding_in_Q903", "wounding_in_H898", "feeding_in_Q903", "feeding_in_H898",
+               "comb_effect_in_Q903", "comb_effect_in_H898", "ctrl_vs_ctrl", "wound_vs_wound",
+               "gallery_vs_gallery", "wounding_diff", "feeding_diff", "combined_diff",
+               "induced_vs_const"))
 
+  options(digits = 10)
   (t_medians <- aggregate(t ~ focus_term, sidf, median))
+  
   test_that("medians of the t statistics are what we expect",
             expect_equal(t_medians$t,
-                         c(0.157361758, 0.01198738, 0.10723831,
-                           -0.15538291, -0.09072355, 0.04817940),
-                         tolerance = 2 * .Machine$double.eps ^ 0.5))
-
+                         c(0.092882336, 0.012555037, -0.155382913, -0.090723545,
+                           0.026698663, -0.086665900,  0.157361758, 0.011987382,
+                           0.107238311, -0.067348886,  0.048179395,  0.005007678,
+                           -0.312069443), tolerance = 1.00293e-07))
+#   ,tolerance = 2 * .Machine$double.eps ^ 0.5))
+  
   return(sidf)
 }
