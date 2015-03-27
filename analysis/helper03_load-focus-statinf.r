4ff8faf9ffb4c86f15bd8d853a764678b102a4bb
mack <yuen.mack@gmail.com>
2015-03-27 08:38:52 -0700

    add back "Wound Response" constrast

--- analysis/helper03_load-focus-statinf.r
+++ analysis/helper03_load-focus-statinf.r
@@ -7,25 +7,21 @@ load_focus_statInf <- function() {
   sidf <- read.delim("../results/limma-results-focus-terms.tsv")
   
   test_that("inference results for our focus terms (still) have 755,352 rows",
-            expect_equal(58104 * 13, nrow(sidf)))
+            expect_equal(58104 * 7, nrow(sidf)))
   
   sidf$focus_term <- 
     factor(sidf$focus_term, levels = 
-             c("wound_Q903", "wound_H898", "feed_Q903", "feed_H898",
-               "combined_effect_Q903", "combined_effect_H898", 
-               "ctrl_vs_ctrl", "wound_vs_wound","gallery_vs_gallery", 
-               "wounding_diff", "feeding_diff", "combined_diff",
-               "induced_vs_const"))
+             c("constDiff", "woundResp_Q903", "woundResp_H898", "weevilInd_Q903",
+               "weevilInd_H898", "weevilCtrl_Q903", "weevilCtrl_H898"))
 
-  options(digits = 10)
+
+  options(digits = 8)
   (t_medians <- aggregate(t ~ focus_term, sidf, median))
   
   test_that("medians of the t statistics are what we expect",
             expect_equal(t_medians$t,
-                         c(0.049744821, -0.002146165, -0.208963248, -0.074254685, 
-                           -0.091773909, -0.093418364, 0.116999851, 0.015444845,
-                           0.198138740, -0.051140065, 0.103334093, 0.059183495,
-                           -0.342118463), 
+                         c(0.11699985, 0.0497448213, -0.0021461646, -0.2089632477,
+                           -0.0742546845, -0.0917739088, -0.0934183638),
                          tolerance = 2 * .Machine$double.eps ^ 0.5))
   
   return(sidf)
