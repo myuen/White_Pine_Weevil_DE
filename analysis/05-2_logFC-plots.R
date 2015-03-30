df8b2d0c37a0e8c3a54d240ce1300562fb7e1b42
Mack Yuen <mack@glauca.msl.ubc.ca>
2015-03-30 09:23:59 -0700

    Clean up codes and pick new color for plot

--- analysis/05-2_logFC-plots.R
+++ analysis/05-2_logFC-plots.R
@@ -1,29 +1,22 @@
 library(ggplot2)
+library(grid)
 
 setwd("analysis/")
 
-# source("helper01_load-counts.r")
-# source("helper02_load-exp-des.r")
 source("helper03_load-focus-statinf.r")
-# source("helper04_extract-and-tidy.r")
-# source("helper05_figures-maker.R")
 source("helper06_classifyCtgs.R")
-
 
 sidf <- load_focus_statInf()
 str(sidf)
 
-
-topN <- 200
 lfc <- 2
 pCutoff <- 0.01
 
-
-## log FC plot of ctrl_vs_ctrl(H898C - Q903C) against combined_effect_Q903 (Q903G - Q903C)
+## log FC plot of ctrl_vs_ctrl(H898C - Q903C) against combined_effect_Q903 (Q903G - Q903W)
 
 # subset data, in the order specificed by the user
-mDat <- subset(sidf, sidf$focus_term == "ctrl_vs_ctrl")
-mDat <- rbind(mDat, subset(sidf, sidf$focus_term == "combined_effect_Q903"))
+mDat <- subset(sidf, sidf$focus_term == "constDiff")
+mDat <- rbind(mDat, subset(sidf, sidf$focus_term == "weevilInd_Q903"))
 summary(mDat$focus_term)
 
 mDat_wide <- reshape(mDat, direction = "wide", 
@@ -46,35 +39,39 @@ mDat_wide$type <-
 
 summary(mDat_wide$type)
 # UpUp    UpDown    DownUp  DownDown SigBoring        NS      NSNS 
-# 120        50        86       805       673     16538     47337
+#   40        86        59       825       666     14490     41938 
 
 
 # Plot
 q <- ggplot(subset(mDat_wide, mDat_wide$type == "NS" | mDat_wide$type == "NSNS"),
-            aes(x = logFC.ctrl_vs_ctrl, y = logFC.combined_effect_Q903)) + 
-  geom_point(color = "gray80", size = 2, alpha = 0.75) + 
+            aes(x = logFC.constDiff, y = logFC.weevilInd_Q903)) + 
+  geom_point(color = "gray90", size = 1, alpha = 0.75) + 
   geom_point(data = subset(mDat_wide, mDat_wide$type != "NS" & mDat_wide$type != "NSNS"),
-             aes(x = logFC.ctrl_vs_ctrl, y = logFC.combined_effect_Q903, color = type),
-             size = 2, alpha = 0.9) + 
+             aes(x = logFC.constDiff, y = logFC.weevilInd_Q903, color = type),
+             size = 1, alpha = 0.9) + 
   scale_colour_manual(
     # Change color of label
-    values = c("#d7191c", "#fdae61", "#2c7bb6", "#a6d96a", "gray50"), 
+    values = c("#b2182b", "#ef8a62", "#67a9cf", "#2166ac", "gray40"), 
     # Change legend label
-    labels = c("Stat. Sig. and Up Reg. in Both Contrasts",
-               "Stat. Sig. Up Reg. in Ctrl vs. Ctrl and Down Reg. in Q903 Feeding", 
-               "Stat. Sig. Down Reg. in Ctrl vs. Ctrl and Up Reg. in Q903 Feeding", 
-               "Stat. Sig. and Down Reg. in Both Contrasts",
-               "Stat. Sig but not Sig. Diff. Expressed"), 
+    labels = 
+      c("Stat. Sig. and Up Reg. in Both Contrasts",
+        "Stat. Sig. Up Reg. in Constitutive Diff. and \nDown Reg. in Q903 Weevil Induced", 
+        "Stat. Sig. Down Reg. in Constitutive Diff. and \nUp Reg. in Q903 Weevil Induced", 
+        "Stat. Sig. and Down Reg. in Both Contrasts",
+        "Stat. Sig but not Sig. Diff. Expressed"), 
     guide = guide_legend(title = NULL)) +
   labs(title = "Expression Fold Change for Control to Control Comparison against 
        Combined Wounding and Feeding Effect in Q903", 
-       x = "Log 2 Fold Change for Control vs Control", 
-       y = "Log 2 Fold Change for Feeding and Wounding Effect in Q903") + 
-  theme_bw() + theme(plot.title = element_text(size = rel(2)), 
-                     legend.position = c(0.8, 0.2)) + 
+       x = "Log 2 Fold Change for Constitutive Difference", 
+       y = "Log 2 Fold Change for Weevil Induced in Q903") + 
+  theme_bw() + 
+  theme(plot.title = element_text(size = rel(2)),
+        legend.position = c(0.88, 0.15),
+        legend.key.height = unit(0.85, "cm")) + 
+  guides(colour = guide_legend(title = NULL, override.aes = list(size = 2))) +
   geom_vline(xintercept = 0, colour = "black") +
   geom_abline(aes(intercept = 0, slope = 0), colour = "black")
 
 
-ggsave("../results/figures/ctrlVsCtrl_To_Q903FeedAndWound.png", 
+ggsave("../results/figures/constDiff_To_Q903WeevilInd.png", 
        plot = q, width = 16, height = 9)
