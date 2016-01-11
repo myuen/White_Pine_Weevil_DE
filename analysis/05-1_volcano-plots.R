4b5a1620341e4f8a1300480b0d0545df8df52fbe
Mack Yuen <mack@glauca.msl.ubc.ca>
2016-01-11 15:40:22 -0800

    Update logFC cutoff variable name to keep consistency with other scripts.

--- analysis/05-1_volcano-plots.R
+++ analysis/05-1_volcano-plots.R
@@ -8,11 +8,12 @@ source("helper05_figures-maker.R")
 sidf <- load_focus_statInf()
 str(sidf)
 
-# LogFC cutoff
-lfc <- 2
-# Adj. p-value cutoff
-pCutoff <- 0.01
+# abs(logFC) log fold change cut-off.  Anything greater than (-1 x lfcCutoff) and less 
+# than lfc will be deemed biological insignificant
+lfcCutoff <- 2
 
+# p-value cut-off.  Anything > pCutoff will be deemed statistically insignificant.
+pCutoff <- 0.01
 
 # You should be able to pass focus_terms <- levels(sidf$focus_term) to lapply
 # but one of our focus_term do not produce any sig. DE contigs and will crash ggplot
@@ -21,7 +22,7 @@ focus_terms <- c("constDiff", "woundResp_Q903",
                   "weevilCtrl_Q903", "weevilCtrl_H898")
 
 lapply(focus_terms, function(x) {
-  p <- volcanoPlot(subset(sidf, sidf$focus_term == x), lfc, pCutoff, x);
+  p <- volcanoPlot(subset(sidf, sidf$focus_term == x), lfcCutoff, pCutoff, x);
   
   ggsave(paste0("../results/figures/", x, "_vPlot.png"), 
          plot = p, height = 8.5, width = 11)
