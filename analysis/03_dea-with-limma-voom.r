680ead15bdefa138eef8cc7c9551124fb1c0ffa4
mack <myuen@mail.ubc.ca>
2016-08-19 15:38:33 -0700

    Clean up codes and comments.  Recode with dplyr and purrr for improve readability.

--- analysis/03_dea-with-limma-voom.r
+++ analysis/03_dea-with-limma-voom.r
@@ -2,9 +2,10 @@
 library(knitr)
 opts_chunk$set(fig.path = 'figure/03-dea-with-limma-voom-')
 
+library(dplyr)
 library(edgeR)
-library(plyr)
-library(reshape2)
+library(purrr)
+library(stringr)
 library(testthat) # facilitate tests that will catch changes on re-analysis
 
 ### Differential Expression Analysis on Sitka Spruce Weevil 
@@ -16,17 +17,21 @@ setwd("analysis/")
 source("helper01_load-counts.r")
 source("helper02_load-exp-des.r")
 
+# abs(logFC) log fold change cut-off.  Anything greater than (-1 x lfc) and less 
+# than lfc will be deemed biological insignificant
+lfcCutoff <- 2
+
+# p-value cut-off.  Anything > pCutoff will be deemed statistically insignificant.
+pCutoff <- 0.05
+
+
 #' Load counts from Sailfish
 x <- load_counts() # takes a few moments
-str(x, list.len = 8) # 'data.frame':  58104 obs. of  24 variables
+str(x, list.len = 8) # 38197 obs. of  24 variables
 
 #' Load experimental design
 expDes <- load_expDes()
-expDes$grp <- # not really sure that we need this?
-  with(expDes, factor(grp,
-                      levels = paste(levels(gType),
-                                     rep(levels(tx), each = 2), sep = ".")))
-str(expDes) # 'data.frame':  24 obs. of  6 variables:
+str(expDes) # 'data.frame':  24 obs. of  6 variables
 
 
 #' Load counts into DGEList object from edgeR package.
@@ -36,16 +41,11 @@ y <- DGEList(counts = x, group = expDes$grp)
 y <- calcNormFactors(y)
 
 #' Write CPM output for heatmap and contig plotting
-write.table(cpm(y), file = "../results/normalized_cpm.txt", quote = FALSE,
+write.table(cpm(y), file = "../results/normalized_cpm.15July.txt", quote = FALSE,
             row.names = TRUE, col.names = TRUE)
 
 #' make model matrix
 modMat <- model.matrix(~ gType * tx, expDes)
-
-# I never needed to fit the model with this parametrization. Instead I got the
-# effects of interests from the main parametrization and specific contrasts
-# specifed below.
-#modMat <- model.matrix(~ tx/gType - 1, expDes)
 
 #' It's hard to believe, but the default names for columns associated with 
 #' interaction terms will create fatal errors in makeContrasts below; prevent 
@@ -67,34 +67,35 @@ cont_matrix <-
   makeContrasts(
     # A - Between genotype comparisons
     # A1 - constitutive difference (H898 control - Q903 control)
-    constDiff = gTypeH898res,
+    constDiff = gTypeH898,
     # B - Within genotype comparisons
     # B1 - Wound Response (Wound - Control)
     woundResp_Q903 = txWound,
-    woundResp_H898 = txWound + gTypeH898res_txWound,
+    woundResp_H898 = txWound + gTypeH898_txWound,
     # B1 - Weevil Induced (Gallery - Wound)
     weevilInd_Q903 = txGallery - txWound,
     weevilInd_H898 = txGallery - txWound +
-      gTypeH898res_txGallery - gTypeH898res_txWound,
+      gTypeH898_txGallery - gTypeH898_txWound,
     # B2 - Weevil Control (Gallery - Control)
     weevilCtrl_Q903 = txGallery,
-    weevilCtrl_H898 = txGallery + gTypeH898res_txGallery,
+    weevilCtrl_H898 = txGallery + gTypeH898_txGallery,
     levels = modMat)
 
 fit2 <- contrasts.fit(fit, cont_matrix)
 
 fit3 <- eBayes(fit2)
 
-summary(decideTests(fit3, p.value = 0.01, lfc = 2))
+summary(decideTests(fit3, p.value = pCutoff, lfc = lfcCutoff))
 #    constDiff woundResp_Q903 woundResp_H898 weevilInd_Q903
-# -1      3377              1              0           4321
-# 0      51092          58103          58104          52302
-# 1       3635              0              0           1481
+# -1      2218              1              0           3690
+# 0      33722          38196          38197          32485
+# 1       2257              0              0           2022
 
 #    weevilInd_H898 weevilCtrl_Q903 weevilCtrl_H898
-# -1              2            3618               2
-# 0           58102           51825           58102
-# 1               0            2661               0
+# -1              0            3853               1
+# 0           38197           31019           38194
+# 1               0            3325               2
+
 
 focus_terms <- colnames(cont_matrix)
 
@@ -103,39 +104,47 @@ names(focus_terms) <- focus_terms
 #' to get individual t statistics, etc., must use topTable() on each coef
 #' separately
 statInf_focus_terms <-
-  adply(focus_terms, 1,
-        function(x) name_rows(topTable(fit3, coef = x,
-                                       number = Inf, sort.by = "none")))
-
-statInf_focus_terms <-
-  rename(statInf_focus_terms, c(X1 = "focus_term", .rownames = "contig"))
-
-statInf_focus_terms <-
-  statInf_focus_terms[ ,c("contig",
-                          setdiff(names(statInf_focus_terms),"contig"))]
-
+  map_df(focus_terms, function(x) {
+    tmp <- topTable(fit3, coef = x, number = Inf, sort.by = "none")
+    tmp$focus_term <- x
+    tmp$cds <- row.names(tmp) 
+    tmp$contig <- str_replace(tmp$cds, ":\\d+\\-\\S+", "")
+    return(tmp)
+    })
 str(statInf_focus_terms)
+# 'data.frame':	267379 obs. of  9 variables:
 
-test_that("stat inf on the focus terms has 406,728 rows",
-          expect_equal(58104 * 7, nrow(statInf_focus_terms)))
+statInf_focus_terms$focus_term <- factor(statInf_focus_terms$focus_term)
 
+# Reorganize columns
+statInf_focus_terms <- 
+  select(statInf_focus_terms, cds, contig, focus_term, logFC, AveExpr, t, P.Value, adj.P.Val, B)
+
+test_that("stat inf on the focus terms has 267379 rows",
+          expect_equal(38197 * 7, nrow(statInf_focus_terms)))
+
+options(digits = 15)
 (t_medians <- aggregate(t ~ focus_term, statInf_focus_terms, median))
 
-all.equal(t_medians$t, c(0.11699985, 0.0497448213, -0.0021461646, -0.2089632477,
-                         -0.0742546845, -0.0917739088, -0.0934183638))
+all.equal(t_medians$t, 
+          c(0.11886468313334492, -0.10327785045908873, -0.00798612294370277,
+            -0.10336498104535216, -0.09816757866141612, -0.00856661400850118,
+            0.04949321203384519))
 
 write.table(statInf_focus_terms,
-            "../results/limma-results-focus-terms.tsv",
+            "../results/limma-results-focus-terms.15July.tsv",
             quote = FALSE, sep = "\t", row.names = FALSE)
 
 statInf_focus_terms.wide <- reshape(statInf_focus_terms, direction = "wide",
-                                    timevar = "focus_term", idvar = "contig",
-                                    new.row.names = unique(statInf_focus_terms$contig),
-                                    drop = c("contig", "AveExpr", "t", "P.Value", "B"))
+                                    timevar = "focus_term", idvar = c("cds"),
+                                    new.row.names = unique(statInf_focus_terms$cds),
+                                    drop = c("AveExpr", "t", "P.Value", "B", "contig"))
 
-statInf_focus_terms.wide <- statInf_focus_terms.wide[, 
-                                   colnames(statInf_focus_terms.wide)[-1]]
+colnames(statInf_focus_terms.wide)[1] <- "contig"
+
+statInf_focus_terms.wide$contig <- 
+  str_replace(row.names(statInf_focus_terms.wide), ":\\d+\\-\\S+", "")
 
 write.table(statInf_focus_terms.wide,
-            "../results/limma-results-focus-terms.wide.tsv",
+            "../results/limma-results-focus-terms.wide.15July.tsv",
             quote = FALSE, sep = "\t", row.names = TRUE)
