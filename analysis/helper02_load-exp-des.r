91171d19bc243321fa3177acf3febfcce953b385
mack <myuen@mail.ubc.ca>
2016-08-19 15:39:40 -0700

    Propagate changes made to genotype to H898 from H898res and Q903 from Q903susc.

--- analysis/helper02_load-exp-des.r
+++ analysis/helper02_load-exp-des.r
@@ -2,16 +2,20 @@
 
 load_expDes <- function() {
   require(testthat)  
-  require(plyr)
-  
+  require(dplyr)
+
   expDes <- read.delim("../data/White_Pine_Weevil_exp_design.tsv",
                        stringsAsFactors = FALSE)
   expDes <-
     mutate(expDes,
-           gType = factor(gType, levels = c("Q903susc", "H898res")),
+           gType = factor(gType, levels = c("Q903", "H898")),
            txCode = factor(txCode, levels = c('C', 'W', 'G')),
-           tx = factor(tx, levels = c("Control", "Wound", "Gallery")))
+           tx = factor(tx, levels = c("Control", "Wound", "Gallery")), 
+           grp = factor(grp, levels = paste(levels(gType),
+                                             rep(levels(tx), each = 2), sep = ".")))
+
   test_that("design matrix has 24 rows upon import",
             expect_equal(24, nrow(expDes)))
+
   return(expDes)
 }
