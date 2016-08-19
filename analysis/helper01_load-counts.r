fd2cdd62c59b930fb11730b6f78e08537e781472
mack <myuen@mail.ubc.ca>
2016-08-19 11:02:26 -0700

    Specify colClasses in read.delim to improve performance

--- analysis/helper01_load-counts.r
+++ analysis/helper01_load-counts.r
@@ -3,9 +3,10 @@
 load_counts <- function() {
   require(testthat)  
   filteredSailfishCounts <- # take a few moments
-    read.delim("../data/consolidated-filtered-Sailfish-results.txt")
-  test_that("filtered Sailfish data has 58104 rows upon import",
-            expect_equal(58104, nrow(filteredSailfishCounts)))
+    read.delim("../data/consolidated-lowExpFiltered-Sailfish-results.15July.txt",
+               colClasses = c("character", rep("numeric", 24)), header = TRUE, row.names = 1)
+  test_that("filtered Sailfish data has 38197 rows upon import",
+            expect_equal(38197, nrow(filteredSailfishCounts)))
   test_that("Sailfish data has data for exactly 24 samples",
             expect_equal(24, ncol(filteredSailfishCounts)))
   return(filteredSailfishCounts)
