6b57b6164161153c793dca8c5b88a846e45de1b9
mack <myuen@mail.ubc.ca>
2016-08-19 10:25:19 -0700

    Remove unused library call. Improve raw data file load speed by specifying class.

--- analysis/02_pre-dea-filtering.r
+++ analysis/02_pre-dea-filtering.r
@@ -1,18 +1,19 @@
 library(edgeR)
 library(ggplot2)
-library(plyr)
 library(testthat) # facilitate tests that will catch changes on re-analysis
 
 
 ### Experiment with limma + voom
 
 # Load counts from Sailfish
-rawSailfishCounts <- read.delim("./data/consolidated-Sailfish-results.txt")
-str(rawSailfishCounts) # 'data.frame':  483047 obs. of  24 variables:
-test_that("Sailfish data has 483047 rows upon import",
-          expect_equal(483047, nrow(rawSailfishCounts)))
+rawSailfishCounts <- read.delim("data/consolidated-Sailfish-results.15July.txt",
+                                colClasses = c("character", rep("numeric", 24)), row.names = 1)
+str(rawSailfishCounts) # 104875 obs. of  24 variables
+test_that("Sailfish data has 104875 rows upon import",
+          expect_equal(104875, nrow(rawSailfishCounts)))
 test_that("Sailfish data has data for exactly 24 samples",
           expect_equal(24, ncol(rawSailfishCounts)))
+
 
 # Load counts into DGEList object from edgeR package.
 y <- DGEList(counts = rawSailfishCounts)
@@ -40,7 +41,7 @@ p <- p + annotate("text", y = Inf, x = 12, hjust = 1.1,
   annotate("text", y = Inf, x = 22.5, hjust = 1.1,
            label = paste(with(present_sample_count, freq[num.present == 24]),
                          'contigs called "present" in all samples'))
-ggsave("figure/02_pre-dea-filtering-preDE-filtering.png", plot = p,
+ggsave("analysis/figure/02_pre-dea-filtering-preDE-filtering.png", plot = p,
        height = 7, width = 7)
 
 ## specifying height and width prevents an empty Rplots.pdf file from being left
@@ -51,10 +52,9 @@ ggsave("figure/02_pre-dea-filtering-preDE-filtering.png", plot = p,
 # We are setting an arbitary threshold and only keeping contigs with more than
 # 1 count-per-million (cpm) in at least 2 samples
 y <- y[(rowSums(cpm(y) > 1) > 2), ]
-test_that("After low expression filter, we have 58104 rows",
-          expect_equal(58104, nrow(y)))
-# 58104 (down from 483047)
+test_that("After low expression filter, we have 38197 rows",
+          expect_equal(38197, nrow(y)))
 
 ## write to file
-write.table(y$counts, "data/consolidated-filtered-Sailfish-results.txt",
+write.table(y$counts, "data/consolidated-lowExpFiltered-Sailfish-results.15July.txt",
             sep = "\t", quote = FALSE)
