61d8db1b1750f07dd684fc2b4022a4eca476bb06
mack <myuen@mail.ubc.ca>
2016-08-19 09:43:13 -0700

    Rewrite some functions with purrr, dplyr and stringr for improved readability. Cleaned up comments.

--- analysis/01_marshal-data.r
+++ analysis/01_marshal-data.r
@@ -1,129 +1,110 @@
+library(purrr)
+library(stringr)
 library(testthat) # facilitate tests that will catch changes on re-analysis
-library(plyr)     ## **ply() and revalue()
+
 
 # Marshal output files from Sailfish
-jFiles <- list.files("data/Sailfish-results/", full.names = TRUE)
+rawFiles <- list.files("data/Sailfish-results-8June2016/", full.names = TRUE)
 test_that("Exactly 24 Sailfish output files are found",
-          expect_equal(24, length(jFiles)))
+          expect_equal(24, length(rawFiles)))
 
 ## Extract, e.g. 898C1 from Sailfish-results/898C1_quant.sf
-tmp <- gsub("([0-9]+[CGW][1234])_quant.sf", "\\1", basename(jFiles))
+tmp <- str_extract(basename(rawFiles), "\\d{3}[A-Z]{1}\\d")
+
 ## Prepend "H" to 898 and "Q" to 903
-tmp <- gsub("898", "H898", tmp)
-tmp <- gsub("903", "Q903", tmp)
+tmp <- str_replace(tmp, "898", "H898")
+tmp <- str_replace(tmp, "903", "Q903")
+
 ## Use as names for good side effects later
-names(jFiles) <- tmp
+names(rawFiles) <- tmp
+
 
 ## Read one file to learn how many rows we expect and to grab rownames
-tmp <- read.table(jFiles[1], row.names = 1,
+tmp <- read.table(rawFiles[1], row.names = 1, header = TRUE,
                   ## specifying colClasses speeds this up 2x
-                  colClasses = rep(c("character", "numeric"), c(1, 6)))
-(n <- nrow(tmp)) # 492317
-jRowNames <- rownames(tmp)
-test_that("First Sailfish output file has 492317 rows",
-          expect_equal(492317, n))
+                  colClasses = rep(c("character", "numeric"), c(1, 4)))
+(n <- nrow(tmp)) # 108152
+rwNames <- rownames(tmp)
+test_that("First Sailfish output file has 108152 rows",
+          expect_equal(108152, n))
 
 
 ## Read in all Sailfish data
 system.time(
-  rawSailfishCounts <-
-    aaply(jFiles, 1, function(x) {
-      jDat <-
-        read.table(x, row.names = 1, nrows = n * 1.1,
-                   ## specifying colClasses speeds this up 2x
-                   colClasses = rep(c("character", "numeric"), c(1, 6)))
-      colnames(jDat) <- scan(x, what = character(), skip = 4, nlines = 1)[c(-1,-2)]
-      test_that("Sailfish output files have expected number of rows",
-                expect_equal(n, nrow(jDat)))
-      return(jDat$EstimatedNumKmers)
-    })
+  myBigList <- map(rawFiles, function(x) {
+    content <- read.table(x, header = TRUE, colClasses = c("character", rep("numeric", 4)))
+
+    # Only extract columns that will be use for analysis
+    content <- select(content, Name, NumReads)
+    # Extract library name from filename
+    libName <- str_extract(x, "\\d{3}[A-Z]\\d")
+    # Pad library name with proper prefix
+    libName <- str_replace(libName, "898", "H898")
+    libName <- str_replace(libName, "903", "Q903")
+
+        # Rename colname to "CDS" and respective library name
+    colnames(content) <- c("CDS", libName)
+    return(content)
+  })
 )
+#    user  system elapsed
+#  21.878   0.062  21.938 
+# purrr not much faster than original implementation with aaply
+str(myBigList) # List of 24
 
-
-## NOTE: rawSailfishCounts is transposed relative to what we expect / want at
-## this point! Will put up with this here and resolve upon export / import.
-colnames(rawSailfishCounts) <- jRowNames
-str(rawSailfishCounts)
-# num [1:24, 1:492317] 0 0 1.87 0 0 ...
-# - attr(*, "dimnames")=List of 2
-# ..$ X1: chr [1:24] "H898C1" "H898C2" "H898C3" "H898C4" ...
-# ..$   : chr [1:492317] "WPW_Inoculation_Trinity_C500_comp100002_c0_seq1" ...
+# Flatten big list of 24 into single data frame
+rawSailfishCounts <- flatten_df(myBigList)
+str(rawSailfishCounts) # Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	108152 obs. of  25 variables:
 
 
 # We discovered more ribosomal RNA post-assembly using BLAST.  The following 
 # line removes putative ribosomal RNA from the table.
-rRNA <- scan("data/putativeRibosomalRNA.id", what = "")
-str(rRNA) # chr [1:389] "WPW_Inoculation_Trinity_C500_comp27782_c0_seq1" ...
-summary(colnames(rawSailfishCounts) %in% rRNA)
+rRNA <- scan("data/ribosomalRNAContaminated.txt", what = "character")
+summary(rawSailfishCounts$CDS %in% rRNA)
 #    Mode   FALSE    TRUE    NA's 
-# logical  491928     389       0 
+# logical  108118      34       0 
 
-microbial <- scan("data/microbialContamination.id", what = "")
-str(microbial) #  chr [1:181] "WPW_Inoculation_Trinity_C500_comp150393_c0_seq1" ...
-summary(colnames(rawSailfishCounts) %in% microbial)
+microbial <- scan("data/microbialContaminated.txt", what = "character")
+summary(rawSailfishCounts$CDS %in% microbial)
 # Mode   FALSE    TRUE    NA's 
-# logical  492136     181       0
+# logical  108142      10       0 
 
-human <- scan("data/humanContamination.id", what = "")
-str(human) # chr [1:224] "WPW_Inoculation_Trinity_C500_comp107462_c0_seq1" ...
-summary(colnames(rawSailfishCounts) %in% human)
-# Mode   FALSE    TRUE    NA's 
-# logical  492093     224       0
-
-weevil <- scan("data/weevilContamination.id", what = "")
-str(weevil) # chr [1:403] "WPW_Inoculation_Trinity_C500_comp100267_c0_seq1" ...
-summary(colnames(rawSailfishCounts) %in% weevil)
-# Mode   FALSE    TRUE    NA's 
-# logical  491914     403       0
-
-fungal <- scan("data/fungalContamination.id", what = "")
-str(fungal) # chr [1:8361] "WPW_Inoculation_Trinity_C500_comp100413_c0_seq1" ...
-summary(colnames(rawSailfishCounts) %in% fungal)
+fungal <- scan("data/fungalContaminated.txt", what = "character")
+summary(rawSailfishCounts$CDS %in% fungal)
 #    Mode   FALSE    TRUE    NA's 
-# logical  483956    8361       0
+# logical  104915    3237       0 
 
-contaminants <- c(rRNA, human, microbial, fungal, weevil)
+contaminants <- c(rRNA, microbial, fungal)
 contaminants <- unique(sort(contaminants))
-str(contaminants) # chr [1:9270] "WPW_Inoculation_Trinity_C500_comp100267_c0_seq1" ...
-summary(colnames(rawSailfishCounts) %in% contaminants)
+length(contaminants)
+# [1] 3277
+summary(rawSailfishCounts$CDS %in% contaminants)
 # Mode   FALSE    TRUE    NA's 
-# logical  483047    9270       0 
-
-rawSailfishCounts <- rawSailfishCounts[, !(colnames(rawSailfishCounts) %in% contaminants)]
-str(rawSailfishCounts) # num [1:24, 1:483047]
-
-(n <- ncol(rawSailfishCounts)) # 483047
-test_that("Sailfish output has 483047 rows after rRNA filtering",
-          expect_equal(483047, n))
+# logical  104875    3277       0 
 
 
-## enact the row / column transposition now
-write.table(t(rawSailfishCounts), "data/consolidated-Sailfish-results.txt",
-            sep = "\t", quote = FALSE)
+rawSailfishCounts <- rawSailfishCounts[!(rawSailfishCounts$CDS %in% contaminants), ]
+str(rawSailfishCounts) # Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	104875 obs. of  25 variables
+summary(rawSailfishCounts$CDS %in% contaminants)
+#    Mode   FALSE    NA's 
+# logical  104875       0 
 
 
-## from JB checking she got same data as original
-# library(tools) # md5sum()
-# all.equal(md5sum("bak/consolidated-Sailfish-results-BAK.txt"),
-#           md5sum("consolidated-Sailfish-results.txt"))
-# [1] "Names: 1 string mismatch"
-# presumably this is just the file name mismatch
-# diff'ing in shell turns up no differences
+write.table(rawSailfishCounts, "data/consolidated-Sailfish-results.15July.txt",
+            sep = "\t", quote = FALSE, row.names = FALSE)
 
 
 ## write design matrix
-expDes <- data.frame(sample = rownames(rawSailfishCounts),
-                     gType = factor(substr(rownames(rawSailfishCounts), 1, 4),
-                                        levels = c("Q903", "H898")),
-                     txCode = factor(substr(rownames(rawSailfishCounts), 5, 5),
-                                     levels = c('C', 'W', 'G')),
+libNames <- colnames(rawSailfishCounts)[-1] # exclude the first one which is colname for CDS
+expDes <- data.frame(sample = libNames, 
+                     gType = factor(str_extract(libNames, "[A-Z]\\d{3}"), levels = c("Q903", "H898")),
+                     txCode = factor(str_sub(libNames, 5, 5), levels = c('C', 'W', 'G')),
                      # C = Control, W = Wounding, G = Gallery 
                      tx = factor(NA),
-                     bioRep = as.numeric(substr(rownames(rawSailfishCounts), 6, 6)))
-expDes$gType <- # H898 = Resistance Genotype; Q903 = Susceptible Genotype
-  revalue(expDes$gType, c('H898' = "H898res", 'Q903' = "Q903susc"))
-expDes$tx <-
-  revalue(expDes$txCode, c('C' = "Control", 'W' = "Wound", 'G' = "Gallery"))
+                     bioRep = as.numeric(str_sub(libNames, 6, 6)))
+
+expDes$tx <- recode(expDes$txCode, 'C' = "Control", 'W' = "Wound", 'G' = "Gallery")
+
 expDes$grp <- with(expDes, interaction(gType, tx))
 expDes
 str(expDes)
